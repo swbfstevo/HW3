c041c3c12facdcdfbe83e671085ad7559ec0f98c
Stephen Olson <sjolson@unomaha.edu>
2017-06-05 20:46:30 +0000

    Finished code for funwithstrings

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,40 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    self.gsub!(/(\W|\d)/, "")
+    self.downcase!
+    if self.reverse == self
+      return true
+    else
+      return false
+    end
   end
   def count_words
-    # your code here
+    words = self.split(" ")
+    words.each do |word|
+      word.downcase!
+      word.gsub!(/[^a-z]/, "")
+    end
+    words.delete("")
+    frequency = Hash.new(0)
+    words.each { |word| frequency[word] += 1}
+    return frequency
   end
   def anagram_groups
-    # your code here
+       final = Array.new(0)
+       words = self.split(" ")
+       words.each do |word|
+         place = []
+          words.each do |word2|
+            if (word.downcase.split("").sort == word2.downcase.split("").sort)
+              place.push(word2)
+            end
+          end
+          final.push(place)
+        end
+              
+      return final
+         
+         
   end
 end
 
